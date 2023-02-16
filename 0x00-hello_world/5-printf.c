966299ec423b2c19dea7fe3f2fa8db783d2df061
Caleb-Sam <sam.caleb0@gmail.com>
2023-02-16 10:40:17 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/*
- * main - A program that prints a line using the print function
- * Return: 0 (Success)
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 
 int main(void)
